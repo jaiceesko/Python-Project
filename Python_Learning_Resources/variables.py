9baa513345079b2d397ce27f501c0cac95554c97
JAINABA CEESAY SKOWRON <jainabaceesay0@gmail.com>
2023-10-19 19:28:26 +0200

    Python Learning Resources

---
+++ Python_Learning_Resources/variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Variables
+
+name = "Jainara"
+print(name)
+
+age = 26
+print(age)
+
+is_female = True
+print(is_female)
+
+weight = 62.5
+print(weight)
+
+
+names = ["Sara", "Joe", "Ivette", "Sheinfeld"]
+print(names)
